f55d25147648b5c13e6030499f5c04d9096821e9
Dendy Darin <dendy050677@gmail.com>
2023-12-01 20:19:39 +0900

    Set driver and envinronment

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    float steerSpeed
+    [SerializeField] float steerSpeed = 100f;
+    [SerializeField] float moveSpeed = 5f;
 
     void Start()
     {
@@ -13,7 +14,9 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0, 0.01f, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
